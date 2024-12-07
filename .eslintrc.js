0ee1bcc1e11bb0a6b158bdb39eaa6e7932473de9
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-12-08 00:27:00 +0900

    feat: estlint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {
+    react: {
+      version: 'detect', // React 버전을 자동으로 감지
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React가 반드시 scope에 있어야 한다는 규칙 비활성화
+  },
 };
